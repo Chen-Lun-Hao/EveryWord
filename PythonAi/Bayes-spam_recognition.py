b233d759950d070de55ec14faed12bec53133f98
萧） <2085127827@qq.com>
2022-01-31 22:54:19 +0800

    python

--- PythonAi/Bayes-spam_recognition.py
+++ PythonAi/Bayes-spam_recognition.py
@@ -19,22 +19,38 @@ def getDateSet(dataPath=''):
 
     # the modle of NaiveBayes
     class NaiveBates:
+        #building sort
         def __init__(self):
             self.__ham_count=0 #the count of normal message
             self.__spam_count=0 #the count of spam message
 
-            self.__ham_words_count=0
-            self.__spam_words_count=0
+            self.__ham_words_count=0 #the count of normal message word
+            self.__spam_words_count=0 #the count of spam message word
 
-            self.__ham_words=list()
-            self.__spam_words=list()
+            self.__ham_words=list() #the words list of normal message
+            self.__spam_words=list() #the words list of spam message
 
-
+            #the set of words that are not repeated in trains
             self.__word_dictionary_set=set()
             self.__word_dictionary_size=0
 
-            self.__ham_map=dict()
-            self.__spam_map=dict()
+            self.__ham_map=dict() #the word frequency statistics in normal message
+            self.__spam_map=dict() #the word frequency statistics in spam message
 
             self.__ham_probability=0.0
             self.__spam_probability=0.0
+
+        #the input is the content of a message
+        def data_preprocess(self, sentence):
+            #converts input to lowercase and replaces special characters with spaces
+            temp_info=re.sub('\W',' ',sentence.lower())
+            #Break it up into words by spaces
+            words=re.split(r'\s+',temp_info)
+            #return all words with length greater than or equal to 3
+            return list(filter(lambda x:len(x)>=3,words)) 
+
+        #trainings model
+        def fit(self, X_train, Y_train):
+            words_line=[]
+            
+        
