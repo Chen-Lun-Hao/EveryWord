9d3c272783253c0d160d0097e5a98fe0d8604da2
萧） <2085127827@qq.com>
2022-02-05 10:05:57 +0800

    wuenda-three

--- PythonAi/Single-hidden-layer-Three.py
+++ PythonAi/Single-hidden-layer-Three.py
@@ -53,8 +53,13 @@ def initialize_parameters(n_x, n_h, n_y):
         n_y - the numbers of the output layer
     return:
         parameters - include the dictionary of parameters:
-            w1 - weight matrix, the dimensionality is ()
-            b1 - partial vector, the dimensionality is ()
-            w2 - weight matrix, the dimensionality is ()
-            b2 -  partial vector, the dimensionality is ()
+            w1 - weight matrix, the dimensionality is (n_h, n_x)
+            b1 - partial vector, the dimensionality is (n_h, 1)
+            w2 - weight matrix, the dimensionality is (n_y, n_h)
+            b2 -  partial vector, the dimensionality is (n_y, 1)
     """
+    np.random.seed(2)
+    w1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros(shape=(n_h, 1))
+    w2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros(shape=(n_y, 1))
