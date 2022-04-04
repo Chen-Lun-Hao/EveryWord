a6cdb7c95ab26e7dac6ecf6d3b9d66251496efd6
萧） <2085127827@qq.com>
2022-04-04 11:03:56 +0800

    HTML

---
+++ lunhao/src/main/java/MymySQL.java
@@ -0,0 +1,10 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import com.mysql.cj.xdevapi.Statement;
+
+public class MymySQL {
+    Statement stmt = null;
+    ResultSet rs = null;
+    Connection conn = null;
+}
